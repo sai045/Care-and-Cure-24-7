7e8e2c468e34d0b6d5112ac039427715336bab5f
sai varshith <saivarshith3041@gmail.com>
2022-07-31 10:51:11 +0530

    deploy

--- main.py
+++ main.py
@@ -174,5 +174,5 @@ def indiaHospitalAPI(input_parameters: indiaHospitalModelInput):
     pincode = input_dictionary['pincode']
 
     hospitals = indiaHospital(pincode)
-    return  hospitals
+    return  "Hospital " + hospitals.iloc[0].Hospital + " State " + hospitals.iloc[0].State + " City " + hospitals.iloc[0].City + " LocalAddress " + hospitals.iloc[0].LocalAddress
 
